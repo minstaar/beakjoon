c3b5c84ad844e8ed1a755f5e9b30784f59c02c53
Minseong Park <99701748+minstar001@users.noreply.github.com>
2024-04-06 21:09:53 +0900

    Backtracking

--- 2024/15686.cpp
+++ 2024/15686.cpp
@@ -1,20 +1,44 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <cmath>
+#define MAX 987654321
 using namespace std;
 
+int n, m, ans;
+vector<bool> index(20);
 vector<pair<int, int>> house, chicken;
-vector<bool> visited
 
-void find(int close)
+
+void dfs(int closeNum, int curClose, int maxClose)
 {
+    if(closeNum == maxClose){
+        int sum = 0;
+        for(int i=0; i<house.size(); i++){
+            int dist = MAX;
+            for(int j=0; j<chicken.size(); j++){
+                if(index[j]) continue;
+                dist = min(dist, abs(chicken[j].first - house[i].first) + abs(chicken[j].second - house[i].second));
+            }
+            sum += dist;
+        }
+        ans = min(ans, sum);
+        return;
+    }
 
+    if(chicken.size() - curClose <= maxClose - closeNum) return;
+    for(int i=curClose+1; i<chicken.size(); i++){
+        index[i] = true;
+        dfs(closeNum + 1, i, maxClose);
+        index[i] = false;
+    }
 }
 
 int main()
 {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-    int n, m; cin>>n>>m;
+    cin>>n>>m;
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             int t; cin>>t;
@@ -23,12 +47,14 @@ int main()
             }
             else if(t == 2){
                 chicken.push_back({i, j});
-                visited.push_back(false);
             }
         }
     }
 
-    find(0);
+    ans = MAX;
+    dfs(0, -1, chicken.size() - m);
+
+    cout<<ans;
 
     return 0;
 }
