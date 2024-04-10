c21a50a0008107abb5e2e0b67f0d1cbb79754bf4
Minseong Park <99701748+Minstar-Park@users.noreply.github.com>
2024-04-10 15:16:07 +0000

    Meet in the Middle

--- 2024/1208.cpp
+++ 2024/1208.cpp
@@ -4,24 +4,24 @@
 using namespace std;
 
 long long n, k, ans;
-vector<long long> arr(40);
+vector<long long> arr(45);
 map<long long, long long> cnt;
 
-void dfs(int s, int e, int p, long long sum)
+void dfs(int d, int p, long long sum)
 {
-    if(s == 0 && p == n/2){
+    if(d == 0 && p == n/2){
         cnt[sum]++;
         return;
     }
-    else if(s == n/2 && p == n){
+    else if(d == 1 && p == n){
         auto iter = cnt.find(k - sum);
         if(iter != cnt.end()){
             ans += iter -> second;
         }
         return;
     }
-    dfs(s, e, p + 1, sum);
-    dfs(s, e, p + 1, sum + arr[p]);
+    dfs(d, p + 1, sum);
+    dfs(d, p + 1, sum + arr[p]);
 }
 
 int main()
@@ -31,8 +31,8 @@ int main()
     cin>>n>>k;
     for(int i=0; i<n; i++) cin>>arr[i];
 
-    dfs(0, n/2, 0, 0);
-    dfs(n/2, n, n/2, 0);
+    dfs(0, 0, 0);
+    dfs(1, n/2, 0);
 
     if(k == 0) ans--;
     cout<<ans<<'\n';
