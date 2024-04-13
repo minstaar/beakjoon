15e4020da750a7b4d4497fb260357524c66893bb
Minseong Park <99701748+Minstar-Park@users.noreply.github.com>
2024-04-13 15:26:26 +0000

    Convex Hull Trick

--- 2024/10067.cpp
+++ 2024/10067.cpp
@@ -1,60 +1,49 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-int n, k, ptr;
-long long arr[100010], psum[100010], dp[210][100010];
+int n, k, ptr, top;
+long long arr[100010], psum[100010], dp[2][100010];
+int track[210][100010];
 const long long INF = 1e10;
 
 struct Line
 {
-    long long m, b;
-    double x;
-    Line(long long _m, long long _b, double _x) : m(_m), b(_b), x(_x) {};
-    long long f(long long x)
+    long long m[100010], b[100010];
+    //double x;
+    int idx[100010];
+    //Line(long long _m, long long _b, double _x, int _idx) : m(_m), b(_b), x(_x), idx(_idx) {};
+
+    void addLine(long long mm, long long bb, int pp)
     {
-        return m * x + b;
+        if(top == 0){
+            m[top] = mm;
+            b[top] = bb;
+            idx[top++]=pp;
+            return;
+        }
+        if(top >= 1 && mm == m[top-1]){
+            idx[top-1] = pp;
+            return;
+        }
+        while(top > 0){
+            if((bb - b[top-1]) * (m[top-2] - m[top -1])  <= (b[top-1] - b[top-2]) * (m[top-1] - mm)) top--;
+            else break;
+        }
+        m[top] = mm;
+        b[top] = bb;
+        idx[top++]=pp;
+        if(ptr >= top) ptr = top - 1;
     }
-};
-vector<Line> lines;
 
-double intersection(Line& f, Line& g)
-{
-    return (double)(g.b - f.b) / (f.m - g.m);
-}
-
-void addLine(long long m, long long b)
-{
-    Line cur(m, b, -INF);
-    if(lines.empty()){
-        lines.push_back(cur);
-        return;
+    pair<long long, int> query(long long x)
+    {
+        while(ptr + 1 < top && m[ptr] * x + b[ptr] < m[ptr+1] * x + b[ptr+1]) ptr++;
+        return {m[ptr] * x + b[ptr], idx[ptr]};
     }
-    while(!lines.empty()){
-        double x = intersection(lines.back(), cur);
-        if(x < lines.back().x || (x == lines.back().x && cur.b > lines.back().b)) lines.pop_back();
-        else break;
-    }
-    cur.x = intersection(lines.back(), cur);
-    lines.push_back(cur);
-    if(ptr >= lines.size()) ptr = lines.size() - 1;
-}
-
-long long slope(int j)
-{
-    return psum[j];
-}
-
-long long inter(int i, int j)
-{
-    return dp[i][j] - psum[j] * psum[j];
-}
-
-long long query(long long x)
-{
-    while(ptr + 1 < lines.size() && lines[ptr+1].x < x) ptr++;
-    return lines[ptr].f(x);
-}
+}lines;
+vector<int> ans;
 
 int main()
 {
@@ -68,16 +57,30 @@ int main()
 
     for(int i=1; i<=k; i++){
         ptr = 0;
-        lines.clear();
-        addLine(0, 0);
+        top = 0;
+        lines.addLine(0, 0, 0);
         for(int j=1; j<=n; j++){
-            dp[i][j] = query(psum[j]);
-            if(query(psum[j])<0) cout<<i<<" "<<j<<endl;
-            addLine(slope(j), inter(i-1, j));
+            pair<long long, int> val = lines.query(psum[j]);
+            dp[i&1][j] = val.first;
+            track[i][j] = val.second;
+            lines.addLine(psum[j], dp[(i-1)&1][j] - psum[j] * psum[j], j);
         }
     }
 
-    cout << dp[k][n] << '\n';
+    cout << dp[k&1][n] << '\n';
+
+    int cur = n;
+    ans.push_back(-1);
+    for(int i=k; i>=1; i--){
+        ans.push_back(track[i][cur]);
+        cur = track[i][cur];
+    }
+    sort(ans.begin(), ans.end());
+    for(int i=1; i<=k; i++){
+        if(ans[i] == 0) ans[i] = 1;
+        if(ans[i] <= ans[i-1]) ans[i] = ans[i-1] + 1;
+    }
+    for(int i=1; i<=k; i++) cout << ans[i]<<" ";
 
     return 0;
 }
