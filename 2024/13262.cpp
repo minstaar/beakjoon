06d80df0757b83a6d1d32761614222809dfbcf3a
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-04-16 14:49:02 +0000

    Monotonic Queue Optimization

---
+++ 2024/13262.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, K;
+long long A[5010], Dp[5010][5010], C[5010][5010];
+long long Q[5010], candidates[5010], front, rear, numOfInterval[5010];
+
+long long cross(long long i, long long j)
+{
+    long long l = j + 1, r = N + 1;
+	while (l < r) {
+		long long m = l + r >> 1;
+		if (C[i][m] <= C[j][m])l = m + 1;
+		else r = m;
+	}
+	return l - 1;
+}
+
+void monotonQueueOpt(int x)
+{
+    candidates[0] = N, Q[0] = 0, front = 0, rear = 1;
+	for (int i=1; i<=N; i++) {
+		while(candidates[front] >= i) front++;
+		Dp[x][i] = C[Q[front]][i];
+		numOfInterval[i] = numOfInterval[Q[front]] + 1;
+		while(front + 1 < rear && cross(Q[rear - 1], i) >= candidates[rear - 2]) rear--;
+		candidates[rear - 1] = cross(Q[rear - 1], i); Q[rear] = i; candidates[rear++] = N;
+	}
+}
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    cin >> N >> K;
+    for(int i=1; i<=N; i++) cin >> A[i];
+    for(int i=1; i<=N; i++){
+        C[i][i] = A[i];
+        for(int j=i+1; j<=N; j++){
+            C[i][j] = C[i][j-1] | A[j];
+        }
+    }
+
+    for(int i=1; i<=K; i++){
+        monotonQueueOpt(i);
+    }
+
+    cout << Dp[K][N] << '\n';
+
+    return 0;
+}
