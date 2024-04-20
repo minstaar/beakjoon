eff961c4b29208ea5c436adc40c5fca3b26346d8
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-04-20 14:39:12 +0000

    Monotone Queue Optimization

--- 2024/13262.cpp
+++ 2024/13262.cpp
@@ -8,7 +8,7 @@ long long Q[5010], candidates[5010], front, rear, numOfInterval[5010];
 
 long long cross(long long x, long long i, long long j)
 {
-    long long l = j + 1, r = N + 1;
+    long long l = j, r = N + 1;
 	while (l < r) {
 		long long m = l + r >> 1;
 		if (Dp[x-1][i] + C[i+1][m] <= Dp[x-1][j] + C[j+1][m]) l = m + 1;
@@ -19,16 +19,19 @@ long long cross(long long x, long long i, long long j)
 
 void monotonQueueOpt(int x)
 {
-    candidates[0] = 0, Q[0] = 0, front = 0, rear = 1;
+    candidates[0] = -1, Q[0] = 0, front = 0, rear = 1;
 	for (int i=1; i<=N; i++){
 		while(candidates[front] >= i) front++;
 		Dp[x][i] = Dp[x-1][Q[front]] + C[Q[front]+1][i];
-        if(Dp[x][i] != comp[x][i]) cout<<Dp[x][i]<<" "<<comp[x][i]<<" "<<x<<" "<<i<<" "<<Dp[x-1][Q[front]]<<" "<<C[Q[front]+1][i]<<endl;
+        if(Dp[x][i] != comp[x][i]){
+            cout<<x<<" "<<i<<endl;
+            cout<<Q[rear-1]<<" "<<i<<" " << cross(x, Q[rear-1], i)<<endl;
+        }
 		numOfInterval[i] = numOfInterval[Q[front]] + 1;
 		while(front + 1 < rear && candidates[rear-2] >= cross(x, Q[rear-1], i)) rear--;
 		candidates[rear-1] = cross(x, Q[rear-1], i);
         Q[rear] = i;
-        candidates[rear++] = 0;
+        candidates[rear++] = -1;
 	}
 }
 
@@ -69,12 +72,13 @@ int main()
     }
     
     for(int i=1; i<K; i++){
-        monotonQueueOpt(i);
-    }
-    
-    for(int i=1; i<K; i++){
         solve(i, 1, N, 1, N);
     }
+
+    for(int i=1; i<K; i++){
+        monotonQueueOpt(i);
+    }
+
     cout<<comp[K-1][N]<<'\n';
 
     cout << Dp[K-1][N] << '\n';
