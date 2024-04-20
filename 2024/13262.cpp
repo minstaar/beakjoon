cc6d8f91070c2234f4d4768dea38dd894aea265f
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-04-20 13:11:53 +0000

    temp

--- 2024/13262.cpp
+++ 2024/13262.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int N, K;
 long long A[5010], Dp[5010][5010], C[5010][5010], comp[5010][5010];
 long long Q[5010], candidates[5010], front, rear, numOfInterval[5010];
-/*
+
 long long cross(long long x, long long i, long long j)
 {
     long long l = j + 1, r = N + 1;
@@ -19,18 +19,19 @@ long long cross(long long x, long long i, long long j)
 
 void monotonQueueOpt(int x)
 {
-    candidates[0] = -1, Q[0] = 0, front = 0, rear = 1;
+    candidates[0] = 0, Q[0] = 0, front = 0, rear = 1;
 	for (int i=1; i<=N; i++){
 		while(candidates[front] >= i) front++;
 		Dp[x][i] = Dp[x-1][Q[front]] + C[Q[front]+1][i];
+        if(Dp[x][i] != comp[x][i]) cout<<Dp[x][i]<<" "<<comp[x][i]<<" "<<x<<" "<<i<<" "<<Dp[x-1][Q[front]]<<" "<<C[Q[front]+1][i]<<endl;
 		numOfInterval[i] = numOfInterval[Q[front]] + 1;
 		while(front + 1 < rear && candidates[rear-2] >= cross(x, Q[rear-1], i)) rear--;
 		candidates[rear-1] = cross(x, Q[rear-1], i);
         Q[rear] = i;
-        candidates[rear++] = -1;
+        candidates[rear++] = 0;
 	}
 }
-*/
+
 void solve(int t, int s, int e, int optl, int optr)
 {
     if(s > e) return;
@@ -63,20 +64,20 @@ int main()
     }
 
     for(int i=1; i<=N; i++){
-        //Dp[0][i] = C[1][i];
+        Dp[0][i] = C[1][i];
         comp[0][i] = C[1][i];
     }
-    /*
+    
     for(int i=1; i<K; i++){
         monotonQueueOpt(i);
     }
-    */
+    
     for(int i=1; i<K; i++){
         solve(i, 1, N, 1, N);
     }
     cout<<comp[K-1][N]<<'\n';
 
-    //cout << Dp[K-1][N] << '\n';
+    cout << Dp[K-1][N] << '\n';
 
     return 0;
 }
