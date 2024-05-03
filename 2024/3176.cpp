07184cf5e150f86d372ccd5da590849459df820c
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-05-03 11:30:30 +0000

    LCA

--- 2024/3176.cpp
+++ 2024/3176.cpp
@@ -9,6 +9,17 @@ typedef pair<int, int> pii;
 vector<pii> adj[100010];
 int parent[100010][MAX], depth[100010], dist[100010];
 
+void makeTree(int cur)
+{
+    for(auto next: adj[cur]){
+        if(depth[next.first] == -1){
+            parent[next.first][0] = cur;
+            depth[next.first] = depth[cur] + 1;
+            dist[next.first]
+        }
+    }
+}
+
 int main()
 {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
@@ -20,4 +31,7 @@ int main()
         adj[v].push_back({u, c});
     }
 
+    memset(parent, -1, sizeof(parent));
+    memset(depth, -1, sizeof(depth));
+    makeTree(1);
 }
