8eee9614e5d0fd10880aaa458715fe3432ea6736
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-06-07 00:46:43 +0000

    BBC

---
+++ 2024/1170.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> adj[210];
+int dfn[210], id;
+bool flag;
+
+int dfs(int prev, int cur)
+{
+    dfn[cur] = ++id;
+    int cycle = 0, f = 0;
+    for(auto next: adj[cur]){
+        if(next == prev) continue;
+        if(dfn[next] == 0){
+            cycle += dfs(cur, next);
+        }
+        else if(dfn[cur] > dfn[next]) cycle++;
+        else if(dfn[cur] < dfn[next]) f++;
+    }
+    if(cycle >= 2) flag = true;
+    return cycle - f;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int N, M; cin >> N >> M;
+    for(int i=0; i<M; i++){
+        int u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    int ans = 0;
+    for(int i=1; i<=N; i++){
+        if(dfn[i] == 0){
+            flag = false;
+            dfs(0, i);
+            if(!flag) ans++;
+        }
+    }
+    cout << ans << '\n';
+
+    return 0;
+}
