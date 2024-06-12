5147dc275fd19142204c2c2d93f69fda802fbfdd
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-06-12 01:14:36 +0000

    2-Sat

--- 2024/2207.cpp
+++ 2024/2207.cpp
@@ -77,3 +77,21 @@ struct twosat{
         return true;
     }
 }twosat;
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int N, M; cin >> N >> M;
+    twosat.init(M);
+    for(int i=0; i<N; i++){
+        int x, y; cin >> x >> y;
+        twosat.addEdge(x, y);
+    }
+    if(twosat.satisfy()){
+        cout << "^_^" << '\n';
+    }
+    else cout << "OTL" << '\n';
+
+    return 0;
+}
