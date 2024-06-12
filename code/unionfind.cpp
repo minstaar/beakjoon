55c4be84518b2f5150649a928fa6261fe0b9d54a
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-06-12 01:19:14 +0000

    DSU

---
+++ code/unionfind.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct UnionFind{
+    vector<int> parent;
+
+    void init(int n){
+        parent = vector<int>(n+1, -1);
+    }
+
+    int Find(int x){
+        return parent[x] < 0 ? x : parent[x] = Find(parent[x]);
+    }
+
+    void Union(int x, int y){
+        x = Find(x), y = Find(y);
+        if(x != y){
+            parent[x] += parent[y];
+            parent[y] = x;
+        }
+    }
+}UF;
