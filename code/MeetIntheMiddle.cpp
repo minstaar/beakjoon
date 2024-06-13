052d37b3febd525133e15550d94fa7d75a690c36
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-06-13 07:21:58 +0000

    MeetintheMiddle

---
+++ code/MeetIntheMiddle.cpp
@@ -0,0 +1,41 @@
+//1450.cpp
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int N, C, arr[35];
+ll ans;
+vector<ll> vec[2];
+
+void solve(int k, int s, int e, int cur, long long sum)
+{
+    if(sum > C) return;
+    if(cur >= e){
+        vec[k].push_back(sum);
+        return;
+    }
+    solve(k, s, e, cur + 1, sum);
+    solve(k, s, e, cur + 1, sum + arr[cur]);
+}
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    cin >> N >> C;
+    for(int i=0; i<N; i++){
+        cin >> arr[i];
+    }
+    
+    solve(0, 0, N/2, 0, 0);
+    solve(1, N/2, N, N/2, 0);
+
+    sort(vec[0].begin(), vec[0].end());
+    for(int i=0; i<vec[1].size(); i++){
+        ans += upper_bound(vec[0].begin(), vec[0].end(), C - vec[1][i]) - vec[0].begin();
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
