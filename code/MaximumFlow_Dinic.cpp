8886599638c0e72783367d78ea638ecf398a277e
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-06-14 04:42:32 +0000

    Reduce memory

--- code/MaximumFlow_Dinic.cpp
+++ code/MaximumFlow_Dinic.cpp
@@ -2,22 +2,23 @@
 using namespace std;
 
 struct Dinic{
+    struct Edge{
+        int next, dual, spare;
+    };
     const int INF = 1e9;
     int n, S, T;
     vector<int> level, work;
-    vector<vector<int>> adj, c, f;
+    vector<vector<Edge>> adj;
 
     void init(int _n, int _S, int _T){
         n = _n, S = _S, T = _T;
-        adj = vector<vector<int>>(n+1);
-        c = vector<vector<int>>(n+1, vector<int>(n+1));
-        f = vector<vector<int>> (n+1, vector<int>(n+1));
+        adj = vector<vector<Edge>>(n+1);
     }
 
-    void addEdge(int u, int v, int w){
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-        c[u][v] = w;
+    void addEdge(int u, int v, int w, bool directed = 1){
+        Edge e1 = {v, adj[v].size(), u}, e2 = {u, adj[u].size(), (directed ? 0 : w)};
+        adj[u].push_back(e1);
+        adj[v].push_back(e2);
     }
 
     bool bfs(){
@@ -28,8 +29,8 @@ struct Dinic{
         while(!que.empty()){
             int cur = que.front();
             que.pop();
-            for(auto next: adj[cur]){
-                if(level[next] == -1 && c[cur][next] - f[cur][next] > 0){
+            for(auto [next, dual, spare]: adj[cur]){
+                if(level[next] == -1 && spare > 0){
                     level[next] = level[cur] + 1;
                     que.push(next);
                 }
@@ -38,15 +39,15 @@ struct Dinic{
         return level[T] != -1;
     }
 
-    int dfs(int cur, int dest, int flow){
-        if(cur == dest) return flow;
+    int dfs(int cur, int flow){
+        if(cur == T) return flow;
         for(int &i=work[cur]; i<adj[cur].size(); i++){
-            int next = adj[cur][i];
-            if(level[next] == level[cur] + 1 && c[cur][next] - f[cur][next] > 0){
-                int df = dfs(next, dest, min(c[cur][next] - f[cur][next], flow));
+            auto [next, dual, spare] = adj[cur][i];
+            if(level[next] == level[cur] + 1 && spare > 0){
+                int df = dfs(next, min(spare, flow));
                 if(df > 0){
-                    f[cur][next] += df;
-                    f[next][cur] -= df;
+                    spare -= df;
+                    adj[next][dual].spare += df;
                     return df;
                 }
             }
@@ -59,7 +60,7 @@ struct Dinic{
         while(bfs()){
             work = vector<int>(n+1);
             while(1){
-                int flow = dfs(S, T, INF);
+                int flow = dfs(S, INF);
                 if(flow == 0) break;
                 total += flow;
             }
