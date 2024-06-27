bd6f6ec0af24b91ebe179375c044124a7072af6e
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-06-27 07:34:06 +0000

    HLD

--- 2024/13512.cpp
+++ 2024/13512.cpp
@@ -17,7 +17,7 @@ template<typename node_t> struct SegmentTree{
 
     void construct(){
         for(int i=sz-1; i>0; i--){
-            tree[i] = max(tree[i<<1], tree[i<<1|1]);
+            tree[i] = tree[i<<1] + tree[i<<1|1];
         }
     }
 
@@ -25,7 +25,7 @@ template<typename node_t> struct SegmentTree{
         --x |= sz;
         tree[x] = val;
         while(x >>= 1){
-            tree[x] = max(tree[x<<1], tree[x<<1|1]);
+            tree[x] = tree[x<<1] + tree[x<<1|1];
         }
     }
 
@@ -34,25 +34,39 @@ template<typename node_t> struct SegmentTree{
         return tree[x];
     }
 
-    node_t query(int l, int r){
+    node_t sum(int l, int r){
         --l |= sz, --r |= sz;
         int res1 = 0, res2 = 0;
         while(l <= r){
-            if(l & 1) res1 = max(res1, tree[l++]);
-            if(~r & 1) res2 = max(res2, tree[r--]);
+            if(l & 1) res1 += tree[l++];
+            if(~r & 1) res2 += tree[r--];
             l >>= 1, r >>= 1;
         }
-        return max(res1, res2);
+        return res1 + res2;
+    }
+
+    node_t query(int l, int r){
+        if(sum(l, r) == 0) return -1;
+        int ret = -1;
+        while(l <= r){
+            int m = l + r >> 1;
+            if(sum(l, m)){
+                ret = m;
+                r = m -1;
+            }
+            else l = m + 1;
+        }
+        return ret;
     }
 };
 
 template<typename T> struct HLD{
-    vector<int> chk, sz, depth, parent, top, in, out;
+    vector<int> sz, depth, parent, top, in, rev, chk, arr;
     vector<vector<int>> input, adj;
     SegmentTree<T> seg;
-    int n, pv;
-
-    HLD(int n): n(n), chk(n+1), sz(n+1), depth(n+1), parent(n+1), top(n+1), in(n+1), out(n+1), input(n+1), adj(n+1){}
+    int n, pv = 0;
+    
+    HLD(int n): n(n), sz(n+1), depth(n+1), parent(n+1), top(n+1), in(n+1), rev(n+1), chk(n+1), arr(n+1), input(n+1), adj(n+1){}
 
     void dfs(int cur = 1){
         chk[cur] = 1;
@@ -77,6 +91,7 @@ template<typename T> struct HLD{
 
     void dfs2(int cur = 1){
         in[cur] = ++pv;
+        rev[pv] = cur;
         for(auto next: adj[cur]){
             top[next] = (next == adj[cur][0] ? top[cur] : next);
             dfs2(next);
@@ -87,26 +102,46 @@ template<typename T> struct HLD{
         top[1] = 1;
         dfs(); dfs1(); dfs2();
         seg.init(n);
-        for(int i=1; i<=n; i++){
-            seg.add(in[i], 0);
-        }
-        seg.construct();
     }
 
-    void update(int i, int val){
-        seg.update(in[i], val);
+    void update(int i){
+        arr[i] ^= 1;
+        seg.update(in[i], arr[i]);
     }
 
-    T query(int a, int b){
-        int ret = 0;
-        while(top[a] ^ top[b]){
-            if(depth[top[a]] < depth[top[b]]) swap(a, b);
-            int st = top[a];
-            ret = max(ret, seg.query(in[st], in[a]));
-            a = parent[st];
+    T query(int v){
+        int ret = -1;
+        while(top[v] ^ top[1]){
+            int st = top[v];
+            int tmp = seg.query(in[st], in[v]);
+            if(tmp > 0) ret = tmp;
+            v = parent[st];
         }
-        if(depth[a] > depth[b]) swap(a, b);
-        ret = max(ret, seg.query(in[adj[a][0]], in[b]));
-        return ret;
+        int tmp = seg.query(in[1], in[v]);
+        if(tmp > 0) ret = tmp;
+        return ret > 0 ? rev[ret] : -1;
     }
 };
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int N; cin >> N;
+    HLD<int> hld(N);
+    for(int i=0; i<N-1; i++){
+        int u, v; cin >> u >> v;
+        hld.input[u].push_back(v);
+        hld.input[v].push_back(u);
+    }
+    hld.init();
+
+    int Q; cin >> Q;
+    while(Q--){
+        int op, a; cin >> op >> a;
+        if(op == 1) hld.update(a);
+        else cout << hld.query(a) << '\n';
+    }
+
+    return 0;
+}
