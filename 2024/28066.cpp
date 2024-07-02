3d747d581969d4e3651d6fc8e87f278833a1c7bd
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-07-02 02:56:06 +0000

    Queue

---
+++ 2024/28066.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int N, K; cin >> N >> K;
+    queue<int> que;
+    for(int i=1; i<=N; i++) que.push(i);
+    while(que.size() >a 1){
+        int t = que.front();
+        for(int i=0; i<K; i++){
+            if(!que.empty()) que.pop();
+        }
+        que.push(t);
+    }
+    cout << que.front() << '\n';
+
+    return 0;
+}
