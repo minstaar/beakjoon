7bde32f61e8da3e13a1a723591b60615dec48d31
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-07-04 00:05:38 +0000

    Ad Hoc

---
+++ 2024/11895.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int N; cin >> N;
+    int check = 0, sum = 0, tmp = 1e9;
+    for(int i=0; i<N; i++){
+        int x; cin >> x;
+        check ^= x;
+        sum += x;
+        tmp = min(tmp, x);
+    }
+    
+    if(!check) cout << sum - tmp << '\n';
+    else cout << 0 << '\n';
+
+    return 0;
+
+}
