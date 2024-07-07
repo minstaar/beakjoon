90fbf9824093348ab39d1fdb27b44f90850b701e
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-07-07 06:12:33 +0000

    version upgrade

--- code/Etc/FastIO.cpp
+++ code/Etc/FastIO.cpp
@@ -1,11 +1,10 @@
 #include <bits/stdc++.h>
-#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 using namespace std;
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 /*
  * Author : jinhan814
- * Date : 2021-04-16
+ * Date : 2021-05-05
  * Source : https://blog.naver.com/jinhan814/222266396476
  * Description : FastIO implementation for cin, cout.
  */
@@ -18,9 +17,9 @@ private:
 	bool __END_FLAG__, __GETLINE_FLAG__;
 public:
 	explicit operator bool() { return !__END_FLAG__; }
-	bool is_blank(char c) { return c == ' ' || c == '\n'; }
-	bool is_end(char c) { return c == '\0'; }
-	char _readChar() {
+	bool IsBlank(char c) { return c == ' ' || c == '\n'; }
+	bool IsEnd(char c) { return c == '\0'; }
+	char _ReadChar() {
 		if (read_idx == next_idx) {
 			next_idx = fread(read_buf, sizeof(char), SZ, stdin);
 			if (next_idx == 0) return 0;
@@ -28,35 +27,35 @@ public:
 		}
 		return read_buf[read_idx++];
 	}
-	char readChar() {
-		char ret = _readChar();
-		while (is_blank(ret)) ret = _readChar();
+	char ReadChar() {
+		char ret = _ReadChar();
+		for (; IsBlank(ret); ret = _ReadChar());
 		return ret;
 	}
-	template<typename T> T readInt() {
-		T ret = 0; char cur = _readChar(); bool flag = 0;
-		while (is_blank(cur)) cur = _readChar();
-		if (cur == '-') flag = 1, cur = _readChar();
-		while (!is_blank(cur) && !is_end(cur)) ret = 10 * ret + cur - '0', cur = _readChar();
-		if (is_end(cur)) __END_FLAG__ = 1;
+	template<typename T> T ReadInt() {
+		T ret = 0; char cur = _ReadChar(); bool flag = 0;
+		for (; IsBlank(cur); cur = _ReadChar());
+		if (cur == '-') flag = 1, cur = _ReadChar();
+		for (; !IsBlank(cur) && !IsEnd(cur); cur = _ReadChar()) ret = 10 * ret + (cur & 15);
+		if (IsEnd(cur)) __END_FLAG__ = 1;
 		return flag ? -ret : ret;
 	}
-	string readString() {
-		string ret; char cur = _readChar();
-		while (is_blank(cur)) cur = _readChar();
-		while (!is_blank(cur) && !is_end(cur)) ret.push_back(cur), cur = _readChar();
-		if (is_end(cur)) __END_FLAG__ = 1;
+	string ReadString() {
+		string ret; char cur = _ReadChar();
+		for (; IsBlank(cur); cur = _ReadChar());
+		for (; !IsBlank(cur) && !IsEnd(cur); cur = _ReadChar()) ret.push_back(cur);
+		if (IsEnd(cur)) __END_FLAG__ = 1;
 		return ret;
 	}
-	double readDouble() {
-		string ret = readString();
+	double ReadDouble() {
+		string ret = ReadString();
 		return stod(ret);
 	}
 	string getline() {
-		string ret; char cur = _readChar();
-		while (cur != '\n' && !is_end(cur)) ret.push_back(cur), cur = _readChar();
+		string ret; char cur = _ReadChar();
+		for (; cur != '\n' && !IsEnd(cur); cur = _ReadChar()) ret.push_back(cur);
 		if (__GETLINE_FLAG__) __END_FLAG__ = 1;
-		if (is_end(cur)) __GETLINE_FLAG__ = 1;
+		if (IsEnd(cur)) __GETLINE_FLAG__ = 1;
 		return ret;
 	}
 	friend INPUT& getline(INPUT& in, string& s) { s = in.getline(); return in; }
@@ -67,45 +66,46 @@ private:
 	char write_buf[SZ];
 	int write_idx;
 public:
-	~OUTPUT() { bflush(); }
-	void bflush() {
+	~OUTPUT() { Flush(); }
+	explicit operator bool() { return 1; }
+	void Flush() {
 		fwrite(write_buf, sizeof(char), write_idx, stdout);
 		write_idx = 0;
 	}
-	void writeChar(char c) {
-		if (write_idx == SZ) bflush();
+	void WriteChar(char c) {
+		if (write_idx == SZ) Flush();
 		write_buf[write_idx++] = c;
 	}
-	template<typename T> int getSize(T n) {
+	template<typename T> int GetSize(T n) {
 		int ret = 1;
-		if (n < 0) n = -n;
-		while (n >= 10) ret++, n /= 10;
+		for (n = n >= 0 ? n : -n; n >= 10; n /= 10) ret++;
 		return ret;
 	}
-	template<typename T> void writeInt(T n) {
-		int sz = getSize(n);
-		if (write_idx + sz >= SZ) bflush();
+	template<typename T> void WriteInt(T n) {
+		int sz = GetSize(n);
+		if (write_idx + sz >= SZ) Flush();
 		if (n < 0) write_buf[write_idx++] = '-', n = -n;
-		for (int i = sz - 1; i >= 0; i--) write_buf[write_idx + i] = n % 10 + '0', n /= 10;
+		for (int i = sz; i-- > 0; n /= 10) write_buf[write_idx + i] = n % 10 | 48;
 		write_idx += sz;
 	}
-	void writeString(string s) { for (auto& c : s) writeChar(c); }
-	void writeDouble(double d) { writeString(to_string(d)); }
+	void WriteString(string s) { for (auto& c : s) WriteChar(c); }
+	void WriteDouble(double d) { WriteString(to_string(d)); }
 } _out;
 
 /* operators */
-INPUT& operator>> (INPUT& in, char& i) { i = in.readChar(); return in; }
-INPUT& operator>> (INPUT& in, string& i) { i = in.readString(); return in; }
-template<typename T, typename std::enable_if_t<is_integral_v<T>>* = nullptr>
-INPUT& operator>> (INPUT& in, T& i) { i = in.readInt<T>(); return in; }
-template<typename T, typename std::enable_if_t<is_floating_point_v<T>>* = nullptr>
-INPUT& operator>> (INPUT& in, T& i) { i = in.readDouble(); return in; }
-OUTPUT& operator<< (OUTPUT& out, char i) { out.writeChar(i); return out; }
-OUTPUT& operator<< (OUTPUT& out, string i) { out.writeString(i); return out; }
-template<typename T, typename std::enable_if_t<is_integral_v<T>>* = nullptr>
-OUTPUT& operator<< (OUTPUT& out, T i) { out.writeInt<T>(i); return out; }
-template<typename T, typename std::enable_if_t<is_floating_point_v<T>>* = nullptr>
-OUTPUT& operator<< (OUTPUT& out, T i) { out.writeDouble(i); return out; }
+INPUT& operator>> (INPUT& in, char& i) { i = in.ReadChar(); return in; }
+INPUT& operator>> (INPUT& in, string& i) { i = in.ReadString(); return in; }
+template<typename T, typename std::enable_if_t<is_arithmetic_v<T>>* = nullptr>
+INPUT& operator>> (INPUT& in, T& i) {
+	if constexpr (is_floating_point_v<T>) i = in.ReadDouble();
+	else if constexpr (is_integral_v<T>) i = in.ReadInt<T>(); return in; }
+
+OUTPUT& operator<< (OUTPUT& out, char i) { out.WriteChar(i); return out; }
+OUTPUT& operator<< (OUTPUT& out, string i) { out.WriteString(i); return out; }
+template<typename T, typename std::enable_if_t<is_arithmetic_v<T>>* = nullptr>
+OUTPUT& operator<< (OUTPUT& out, T i) {
+	if constexpr (is_floating_point_v<T>) out.WriteDouble(i);
+	else if constexpr (is_integral_v<T>) out.WriteInt<T>(i); return out; }
 
 /* macros */
 #define fastio 1
@@ -116,10 +116,9 @@ OUTPUT& operator<< (OUTPUT& out, T i) { out.writeDouble(i); return out; }
 /////////////////////////////////////////////////////////////////////////////////////////////
 
 int main() {
-	fastio;
-	int n; cin >> n;
-	while (n--) {
-		int a, b; cin >> a >> b;
-		cout << a + b << '\n';
-	}
+	int a, b;
+	cin >> a >> b;
+	cout << a + b;
+
+    return 0;
 }
