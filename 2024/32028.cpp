2b2f2cd98931bff3dd79910d3d95f53c96656bed
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-07-15 14:19:35 +0000

    Greedy

--- 2024/32028.cpp
+++ 2024/32028.cpp
@@ -2,89 +2,82 @@
 using namespace std;
 using pii = pair<int, int>;
 
+int val[200010];
+pii childVal[200010], childIdx[200010];
+vector<pii> depth[200010];
 
-struct Node
-{
-    int l = -1, r = -1, val = 0, idx;
-};
-
-struct Data
-{
-    int depth, val, idx;
-    bool operator < (const Data &o) const{
-        if(depth == o.depth){
-            return val < o.val;
-        }
-        return depth < o.depth;
-    }
-};
-
-vector<Node> tree(200010);
-int idx[200010], pv;
-
-int update(int cur, int val, int depth, int v)
-{
-    int it = 1;
-    while(it <= depth){
-        if(tree[cur].val > val){
-            if(tree[cur].l == -1){
-                tree[cur].l = ++pv;
-                tree[pv] = {-1, -1, val, v};
-                idx[v] = pv;
-                return it + 1;
-            }
-            else cur = tree[cur].l;
-        }
-        else{
-            if(tree[cur].r == -1){
-                tree[cur].r = ++pv;
-                tree[pv] = {-1, -1, val, v};
-                idx[v] = pv;
-                return it + 1;
-            }
-            else cur = tree[cur].r;
-        }
-        it++;
-    }
-}
 
 int main()
 {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
     int N; cin >> N;
-    vector<Data> arr(N);
-    for(int i=0; i<N; i++){
-        cin >> arr[i].val >> arr[i].depth;
-        arr[i].idx = i;
+
+    for(int i=1; i<=N; i++){
+        int v, d;
+        cin >> v >> d;
+        val[i] = v;
+        childVal[i] = {-2e9 - 1, 2e9 + 1};
+        depth[d].push_back({v, i});
     }
-    sort(arr.begin(), arr.end());
-    bool flag = true;
-    if(arr[0].depth == 1){
-        tree[0] = {-1, -1, arr[0].val};
-        idx[arr[0].idx] = 0;
-    }
-    else{
+
+    if(depth[1].size() != 1){
         cout << -1 << '\n';
         return 0;
     }
+
+    for(int i=1; i<=N; i++) sort(depth[i].begin(), depth[i].end());
+
     for(int i=1; i<N; i++){
-        if(arr[i].depth != update(0, arr[i].val, arr[i].depth, arr[i].idx)){
-            flag = false;
-            break;
+        auto cur = depth[i].begin();
+        auto next = depth[i+1].begin();
+        while(cur != depth[i].end() && next != depth[i+1].end()){
+            int curIdx = cur->second;
+            int nextIdx = next->second;
+            if(childIdx[curIdx].first == 0){
+                int lVal = childVal[curIdx].first;
+                int rVal = val[curIdx];
+                if(val[nextIdx] < lVal){
+                    cout << -1 << '\n';
+                    return 0;
+                }
+                if(val[nextIdx] > rVal){
+                    childIdx[curIdx].first = -1;
+                    continue;
+                }
+                childIdx[curIdx].first = nextIdx;
+                childVal[nextIdx] = {lVal, rVal};
+                next++;
+            }
+            else{
+                int lVal = val[curIdx];
+                int rVal = childVal[curIdx].second;
+                if(val[nextIdx] < lVal){
+                    cout << -1 << '\n';
+                    return 0;
+                }
+                if(val[nextIdx] > rVal){
+                    childIdx[curIdx].second = -1;
+                    cur++;
+                    continue;
+                }
+                childIdx[curIdx].second = nextIdx;
+                childVal[nextIdx] = {lVal, rVal};
+                cur++, next++;
+            }
+        }
+        if(next != depth[i+1].end()){
+            cout << -1 << '\n';
+            return 0;
         }
     }
-    if(flag){
-        for(int i=0; i<N; i++){
-            int lc, rc;
-            if(tree[idx[i]].l == -1) lc = -1;
-            else lc = tree[tree[idx[i]].l].idx + 1;
-            if(tree[idx[i]].r == -1) rc = -1;
-            else rc = tree[tree[idx[i]].r].idx + 1;
-            cout << lc << ' ' << rc << '\n';
-        }
+
+    for(int i=1; i<=N; i++){
+        if(childIdx[i].first == 0) cout << -1 << ' ';
+        else cout << childIdx[i].first << ' ';
+        if(childIdx[i].second == 0) cout << -1 << '\n';
+        else cout << childIdx[i].second << '\n';
     }
-    else cout << -1 << '\n';
 
     return 0;
 }
