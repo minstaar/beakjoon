4662eaf4e4bdf0f1959306040c7754f1910b51b1
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-07-16 10:57:12 +0000

    Add multiset

--- code/DataSructure/OrderedSet.cpp
+++ code/DataSructure/OrderedSet.cpp
@@ -7,3 +7,11 @@ typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_
 ordered_set s;
 //auto x = s.find_by_order(k);
 //s.order_of_key(k);
+
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
+
+void m_erase(ordered_multiset &OS, int val){
+    int index = OS.order_of_key(val);
+    ordered_multiset::iterator it = OS.find_by_order(index);
+    if(*it == val) OS.erase(it);
+}
