cb1b6063e2adbda7b05605a025f539529a4e4407
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-08-10 21:20:54 +0000

    Ad Hoc

---
+++ 2024/12796.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int K; cin >> K;
+    cout << "3\n" << "1 1 1 " << K + 1 << '\n';
+
+    return 0;
+}
