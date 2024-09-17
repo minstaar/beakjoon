4e7b8cd1d08d73ba3a349bb35c3d92e8f86abc66
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-09-17 14:26:31 +0000

    DP & Prefix Sum

--- 2024/32221.cpp
+++ 2024/32221.cpp
@@ -1,35 +1,41 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
+using ll = long long;
 
-const int MOD = 1e9 + 7;
-int N, K, arr[3010], dp[3010][2];
+const ll MOD = 1e9 + 7;
+int N, K;
+bool arr[1000010];
+ll dp[1000010][2], psum[1000010][2];
 
 int main()
 {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
     cin >> N >> K;
-    for(int i=1; i<=N; i++) cin >> arr[i];
+    for(int i=2; i<=N+1; i++) cin >> arr[i];
 
-    dp[0][1] = dp[1][0] = dp[K][1] = 1;
-    for(int i=2; i<=N; i++){
-        dp[i][0] = 1;
-        if(arr[i] != 1){
-            dp[i][0] += dp[i-1][0];
-            dp[i][1] += dp[i-1][1];
+    dp[0][0] = psum[0][0] = 1;
+    for(int i=1; i<=N+1; i++){
+        if(i <= K){
+            dp[i][0] = psum[i-1][0];
+            dp[i][1] = psum[i-1][1];
         }
-        for(int j=i-2; j>=i-K && j>=1; j--){
-            if(arr[j+1] != 1) dp[i][0] = (dp[i][0] + dp[j][0]) % MOD;
-            if(arr[j+1] != 1) dp[i][1] = (dp[i][1] + dp[j][1]) % MOD;
+        else{
+            dp[i][0] = (psum[i-1][0] - psum[i-K-1][0] + MOD) % MOD;
+            dp[i][1] = (psum[i-1][1] - psum[i-K-1][1] + MOD) % MOD;
         }
-        if(i - K - 1 >= 0 && arr[i-K] != 1){
-            dp[i][1] = (dp[i][1] + dp[i-K-1][1]) % MOD;
+        if(i - K >= 1 && arr[i-K] != 1){
             dp[i][1] = (dp[i][1] + dp[i-K-1][0]) % MOD;
+            dp[i][1] = (dp[i][1] + dp[i-K-1][1]) % MOD;
+        }
+        psum[i][0] = psum[i-1][0], psum[i][1] = psum[i-1][1];
+        if(arr[i+1] != 1){
+            psum[i][0] = (psum[i][0] + dp[i][0]) % MOD;
+            psum[i][1] = (psum[i][1] + dp[i][1]) % MOD;
         }
     }
 
-    cout << dp[N][1];
+    cout << dp[N+1][1] << '\n';
 
     return 0; 
 }
