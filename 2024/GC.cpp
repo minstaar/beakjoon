65312084b9df3f9ccd1177f5ace799718179e2c8
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-09-21 08:51:57 +0000

    Game Theory

---
+++ 2024/GC.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int N; cin >> N;
+    if(N == 3) cout << "Hobanwoo\n";
+    else cout << "Sangho\n";
+
+    return 0;
+}
