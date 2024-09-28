11f3617e800a75d4c95c2e57099822ab697eb9e4
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-09-28 07:20:10 +0000

    Geomerty & Binary Search

---
+++ 2024/IH.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int N, K; cin >> N >> K;
+    vector<double> arr(N);
+    for(int i=0; i<N; i++){
+        double x, y; cin >> x >> y;
+        double dist = sqrt(x * x + y * y);
+        arr[i] = dist;
+    }
+    sort(arr.begin(), arr.end());
+
+    int mx = 0;
+    for(int i=0; i<N; i++){
+        int cnt = upper_bound(arr.begin(), arr.end(), arr[i] + K) - arr.begin() - i;
+        mx = max(mx, cnt);
+        cnt = i - (lower_bound(arr.begin(), arr.end(), arr[i] - K) - arr.begin()) + 1;
+        mx = max(mx, cnt);
+        cnt = upper_bound(arr.begin(), arr.end(), arr[i] + (double)K / 2) - lower_bound(arr.begin(), arr.end(), arr[i] - (double)K / 2);
+        mx = max(mx, cnt);
+    }
+
+    double ans = (double)100 / N * mx;
+    cout << fixed;
+    cout.precision(6);
+    cout << ans << '\n';
+
+    return 0;
+}
