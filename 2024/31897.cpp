f1ee9e71c462ec78274003a90c3375a4277ff925
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-09-29 14:37:37 +0000

    Linear Argebra

--- 2024/31897.cpp
+++ 2024/31897.cpp
@@ -26,17 +26,49 @@ int main()
     cin >> res;
     query.push_back({1, 1, 1, res});
     
-    cout << "2 0 1" << endl;
+    cout << "1 2 3" << endl;
     cin >> res;
-    query.push_back({2, 0, 1, res});
+    query.push_back({1, 2, 3, res});
 
     for(int i=0; i<5; i++){ //consider ith query is false
-        bool valid = true;
         int x = -1, y = -1, z = -1;
-        for(int j=0; j<5; j++){
+        for(int j=0; j<3; j++){
             if(i == j) continue;
             auto [a, b, c, res] = query[j];
-            
+            if(a == 1) x = res;
+            if(b == 1) y = res;
+            if(c == 1) z = res;
+        }
+        if(i == 0){
+            x = get<3>(query[3]) - y - z;
+            if(x + 2 * y + 3 * z == get<3>(query[4])){
+                cout << x << ' ' << y << ' ' << z << endl;
+                break;
+            }
+        }
+        if(i == 1){
+            y = get<3>(query[3]) - x - z;
+            if(x + 2 * y + 3 * z == get<3>(query[4])){
+                cout << x << ' ' << y << ' ' << z << endl;
+                break;
+            }
+        }
+        if(i == 2){
+            z = get<3>(query[3]) - x - y;
+            if(x + 2 * y + 3 * z == get<3>(query[4])){
+                cout << x << ' ' << y << ' ' << z << endl;
+                break;
+            }
+        }
+        if(i == 3 && x + 2 * y + 3 * z == get<3>(query[4])){
+            cout << x << ' ' << y << ' ' << z << endl;
+            break;
+        }
+        if(i == 4 && x + y + z == get<3>(query[3])){
+            cout << x << ' ' << y << ' ' << z << endl;
+            break;
         }
     }
+
+    return 0;
 }
