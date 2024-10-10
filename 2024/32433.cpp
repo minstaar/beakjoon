8fa767238246ad687504505dd620d8fc64823e53
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-10-10 10:29:16 +0000

    Bitmasking

---
+++ 2024/32433.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int N, cnt[35];
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    cin >> N;
+    for(int i=0; i<N; i++){
+        int x; cin >> x;
+        for(int j=0; j<=32; j++){
+            if(x & (1 << j)) cnt[j]++;
+        }
+    }
+    for(int i=0; i<N; i++){
+        int res = 0;
+        for(int j=0; j<=32; j++){
+            if(cnt[j]){
+                res |= (1 << j);
+                cnt[j]--;
+            }
+        }
+        cout << res << ' ';
+    }
+
+
+    return 0;
+}
