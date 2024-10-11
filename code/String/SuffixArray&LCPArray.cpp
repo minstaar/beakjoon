4cb17d731e9e6f5d2d89050364669ea34e3da3b1
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-10-11 15:35:40 +0000

    Suffix Array & LCP Array

--- code/String/SuffixArray&LCPArray.cpp
+++ code/String/SuffixArray&LCPArray.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> constructSA(string &str)
+vector<int> constructSA(string &s)
 {
-    int N = str.length(), M = max(256, N) + 1;
+    int N = s.size(), M = max(256, N) + 1;
     vector<int> sa(N), r(N*2), nr(N*2), cnt(M), idx(N);
 
-    for(int i=0; i<N; i++) sa[i] = i, r[i] = str[i];
+    for(int i=0; i<N; i++) sa[i] = i, r[i] = s[i];
     for(int d=1; d<N; d<<=1){
         auto cmp = [&](int i, int j){
             return r[i] < r[j] || (r[i] == r[j] && r[i+d] < r[j+d]);
@@ -22,23 +22,23 @@ vector<int> constructSA(string &str)
         for(int i=1; i<M; i++) cnt[i] += cnt[i-1];
         for(int i=N-1; i>=0; i--) sa[--cnt[r[idx[i]]]] = idx[i];
 
-        nr[sa[0]] = 0;
+        nr[sa[0]] = 1;
         for(int i=1; i<N; i++) nr[sa[i]] = nr[sa[i-1]] + cmp(sa[i-1], sa[i]);
         for(int i=0; i<N; i++) r[i] = nr[i];
 
-        if(r[sa[N-1]] == N - 1) break;
+        if(r[sa[N-1]] == N) break;
     }
 
     return sa;
 }
 
-vector<int> constructLCP(string &str, vector<int> &sa)
+vector<int> constructLCP(string &s, vector<int> &sa)
 {
-    int N = str.length();
+    int N = s.size();
     vector<int> lcp(N), isa(N);
     for(int i=0; i<N; i++) isa[sa[i]] = i;
     for(int i=0, k=0; i<N; i++, k=max(k-1, 0)) if(isa[i]){
-        for(int j=sa[isa[i]-1]; str[i+k]==str[j+k]; k++);
+        for(int j=sa[isa[i]-1]; s[i+k]==s[j+k]; k++);
         lcp[isa[i]] = k;
     }
     return lcp;
