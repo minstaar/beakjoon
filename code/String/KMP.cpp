f3dcc2ad5ba3e1598bad895855cedf1cac16fbc3
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-10-13 07:58:26 +0000

    KMP

--- code/String/KMP.cpp
+++ code/String/KMP.cpp
@@ -1,27 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> getPi(string p)
+vector<int> getPi(string &p)
 {
-    int m = p.length(), j=0;
-    vector<int> pi(m);
-    for(int i=1; i<m; i++){
-        while(j > 0 && p[i] != p[j]) j = pi[j-1];
+    int N = p.size(), j = 0;
+    vector<int> pi(N);
+    for(int i=1; i<N; i++){
+        while(j && p[i] != p[j]) j = pi[j-1];
         if(p[i] == p[j]) pi[i] = ++j;
     }
     return pi;
 }
 
-vector<int> KMP(string s, string p)
+vector<int> KMP(string &s, string &p)
 {
     vector<int> res;
-    int n = s.length(), m = p.length(), j = 0;
+    int N = s.size(), M = p.size(), j = 0;
     auto pi = getPi(p);
-    for(int i=0; i<n; i++){
+    for(int i=0; i<N; i++){
         while(j > 0 && s[i] != p[j]) j = pi[j-1];
         if(s[i] == p[j]){
-            if(j == m - 1){
-                res.push_back(i - m + 1);
+            if(j == M - 1){
+                res.push_back(i - M + 1);
                 j = pi[j];
             }
             else j++;
