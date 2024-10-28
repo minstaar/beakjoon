09b42f9c95958c2ec931cbcaf5ff3ad30816052d
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-10-28 01:34:01 +0000

    Case Work & Constructive & String

---
+++ 2024/1677.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+string A, B, C;
+int dp[110][110][110][8];
+
+int solve(int i, int j, int k, int state)
+{
+    if(i >= A.size() && j >= B.size() && k >= C.size()) return 0;
+
+    int &ret = dp[i][j][k][state];
+    if(dp[i][j][k][state] != -1) return ret;
+
+    int s1 = state & 1, s2 = (state >> 1) & 1, s3 = (state >> 2) & 1;
+    for(int l=0; l<7; l++){
+        int t1 = l & 1, t2 = (l >> 1) & 1, t3 = (l >> 2) & 1;
+        if((s1 && t1) || (s2 && t2) || (s3 && t3)) continue;
+        if(i >= A.size() && !t1 || j >= B.size() && !t2 || k >= C.size() && !t3) continue;
+
+        int tmp = solve(i + !t1, j + !t2, k + !t3, l);
+        if(tmp == 11){
+            int abs = 1;
+        }
+        if(tmp == -1) continue;
+        
+        if(t1 + t2 + t3 == 1){
+            if(t1 && B[j] == C[k]) tmp++;
+            else if(t2 && A[i] == C[k]) tmp++;
+            else if(t3 && A[i] == B[j]) tmp++;
+        }
+        else if(t1 + t2 + t3 == 0){
+            if(A[i] == B[j] && B[j] == C[k]){
+                tmp += 3;
+            }
+            else if(A[i] == B[j] || B[j] == C[k] || C[k] == A[i]) tmp++;
+        }
+        ret = max(ret, tmp);
+    }
+
+    return ret;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    cin >> A >> B >> C;
+    memset(dp, -1, sizeof(dp));
+    cout << solve(0, 0, 0, 0) << '\n';
+
+    return 0;
+}
---
+++ 2024/32526.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+    int N, K; cin >> N >> K;
+    
+    if(K == N - 1){
+        cout << "Yes\n";
+        for(int i=0; i<N; i++) cout << 'a';
+    }
+    else if(K == 0){
+        if(N == 2) cout << "No\n";
+        else{
+            cout << "Yes\n";
+            char a = 'a';
+            for(int i=0; i<N; i++){
+                cout << a;
+                a = (a == 'z' ? 'a' : a + 1);
+            }
+        }
+    }
+    else if(N % K == 0){ // N = 9, K = 3: aabaabaab
+        cout << "Yes\n";
+        string str = "";
+        int M = N / K;
+        for(int i=0; i<M/2; i++) str.push_back('a');
+        for(int i=M/2; i<M; i++) str.push_back('b');
+        for(int i=0; i<K; i++) cout << str;
+    }
+    else if(N % (K + 1) == 0){ // N = 9, K = 2: abaabaaba
+        int M = N / (K + 1);
+        if(M == 2) cout << "No\n";
+        else{
+            cout << "Yes\n";
+            string str = "";
+            for(int i=0; i<(M-1)/2; i++) str.push_back('a');
+            if(M % 2 == 0) str.append("bb");
+            else str.push_back('b');
+            for(int i=str.size(); i<M; i++) str.push_back('a');
+            for(int i=0; i<K+1; i++) cout << str;
+        }
+    }
+    else cout << "No\n";
+    
+    return 0;
+}
