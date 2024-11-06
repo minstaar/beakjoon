8603082f889fbfe7288d3d8a31f5376b0ef74a88
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-11-06 14:54:14 +0000

    Line Intersection

---
+++ code/Geometry/LineIntersection.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pll = pair<ll, ll>;
+
+int CCW(pll &A, pll &B, pll &C)
+{
+    ll op = A.first * B.second + B.first * C.second + C.first * A.second;
+    op -= B.first * A.second + C.first * B.second + A.first * C.second;
+
+    if(op > 0) return 1;
+    else if(op == 0) return 0;
+    else return -1;
+}
+
+bool isIntersect(pll &A, pll &B, pll &C, pll &D)
+{
+    int line1_2 = CCW(A, B, C) * CCW(A, B, D);
+    int line2_1 = CCW(C, D, A) * CCW(C, D, B);
+
+    if(line1_2 == 0 && line2_1 == 0){
+        if(A > B) swap(A, B);
+        if(C > D) swap(C, D);
+        return A <= D && C <= B;
+    }
+    return line1_2 < 0 && line2_1 < 0;
+}
