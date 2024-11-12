56a605c9d34a850028b92dcc5cc5ff92bc226854
Minseong Park <99701748+minstaar@users.noreply.github.com>
2024-11-12 11:09:14 +0000

    Euclidean

--- 2024/32633.cpp
+++ 2024/32633.cpp
@@ -8,7 +8,9 @@ int main()
 {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-    int N; ll L; cin >> N >> L;
+    int N;
+    ll L;
+    cin >> N >> L;
     vector<ll> A(N); for(int i=0; i<N; i++) cin >> A[i];
     vector<int> B(N); for(int i=0; i<N; i++) cin >> B[i];
     ll res = 1;
